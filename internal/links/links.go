15e7d8fe671cea31b6402c0b146d1241efa6e5ab
kodmm <53547701+kodmm@users.noreply.github.com>
2022-07-02 20:02:05 +0900

    completed to Create and Retrieve Links

---
+++ internal/links/links.go
@@ -0,0 +1,56 @@
+package links
+
+import (
+	"log"
+
+	database "github.com/kodmm/hackernews/internal/pkg/db/postgres"
+	"github.com/kodmm/hackernews/internal/users"
+)
+
+type Link struct {
+	ID      string
+	Title   string
+	Address string
+	User    *users.User
+}
+
+func (link Link) Save() int64 {
+	stmt, err := database.Db.Prepare("INSERT INTO Links(Title, Address) VALUES($1, $2) RETURNING ID")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var id int64
+	err = stmt.QueryRow(link.Title, link.Address).Scan(&id)
+	if err != nil {
+		log.Fatal("Error:", err.Error())
+	}
+	log.Print("Row inserted!")
+	return id
+}
+
+func GetAll() []Link {
+	stmt, err := database.Db.Prepare("SELECT id, title, address FROM Links")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var links []Link
+	for rows.Next() {
+		var link Link
+		err := rows.Scan(&link.ID, &link.Title, &link.Address)
+		if err != nil {
+			log.Fatal(err)
+		}
+		links = append(links, link)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return links
+}
---
+++ internal/users/users.go
@@ -0,0 +1,8 @@
+package users
+
+type User struct {
+	ID string `json:"id"`
+	Username string `json:"name"`
+	Password string `json:"password"`
+}
+
